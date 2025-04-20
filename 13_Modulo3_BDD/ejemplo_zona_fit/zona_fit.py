6e1b893cb218eb8605659847bb7071b12ce1ad69
DarkCastor111 <tempo.cat13@gmail.com>
2025-04-20 09:55:07 +0100

    Version 2.3.2 Zona Fit Menu

---
+++ 13_Modulo3_BDD/ejemplo_zona_fit/zona_fit.py
@@ -0,0 +1,71 @@
+from cliente_dao import ClienteDAO
+from cliente import Cliente
+
+class ZonaFit:
+    def iniciar(self):
+        salir = False
+        print('### Gestión de Zona Fit ###')
+        while not salir:
+            try:
+                opcion = self.mostrar_menu()
+                salir = self.ejecutar_opcion(opcion)
+            except Exception as e:
+                print(f'Ocurrió un error : {e}')
+    
+    def mostrar_menu(self):
+        print (f'''Menu:
+        1. Listar Clientes
+        2. Añadir un Cliente
+        3. Actualizar un Cliente
+        4. Eliminar un Cliente
+        9. Salir
+        ''')
+        return int(input('Elije una opción: '))
+    
+    def ejecutar_opcion(self, opcion):
+        if opcion == 1:
+            listado_clientes = ClienteDAO.seleccionar_todos()
+            for cli in listado_clientes:
+                print(cli)
+        elif opcion == 2:
+            print('## Añadir un cliente ##')
+            nom = input('Nombre del nuevo cliente: ')
+            apel = input('Apellido del nuevo cliente: ')
+            mem = input('Membresía del nuevo cliente: ')
+            nuevo_cliente = Cliente(p_nombre=nom, p_apellido=apel, p_membresia=mem)
+            ClienteDAO.crear(nuevo_cliente)
+            print('==> Cliente añadido correctamente!')
+        elif opcion == 3:
+            print('## Modificar un cliente ##')
+            id = input('id del cliente a actualizar: ')
+            nom = input('Nombre del cliente a actualizar: ')
+            apel = input('Apellido del cliente a actualizar: ')
+            mem = input('Membresía del cliente a actualizar: ')
+            cliente_a_actualizar = Cliente(p_nombre=nom, p_apellido=apel, p_membresia=mem, p_id=id)
+            ClienteDAO.actualizar(cliente_a_actualizar)
+            print('==> Cliente actualizado correctamente!')
+        elif opcion == 4:
+            print('## Eliminar un cliente ##')
+            id = input('id del cliente a eliminar: ')
+            cliente_a_eliminar = Cliente(p_id=id)
+            ClienteDAO.eliminar_uno(cliente_a_eliminar)
+            print('==> Cliente eliminado correctamente!')
+        elif opcion == 9:
+            print('Regresa Pronto !!')
+            return True
+        else:
+            print(f'Opcion inválida: {opcion}')
+        return False
+
+
+# Principal
+if __name__ == '__main__':
+    catalogo_peli = ZonaFit()
+    catalogo_peli.iniciar()
+
+
+
+
+
+
+

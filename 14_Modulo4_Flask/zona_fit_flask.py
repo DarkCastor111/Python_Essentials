04871af53a31428f7ee8f6b627ba311db384c04d
DarkCastor111 <tempo.cat13@gmail.com>
2025-04-23 10:52:16 +0100

    Version 2.4.1 Diseño Web

--- 14_Modulo4_Flask/zona_fit_flask.py
+++ 14_Modulo4_Flask/zona_fit_flask.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 servidor_flask = Flask(__name__)
+
+titulo_app = 'Zona Fit (GYM)'
 
 @servidor_flask.route('/') # url http://localhost:5000/
 @servidor_flask.route('/index.html') # url http://localhost:5000/index.html
 def inicio():
-    servidor_flask.logger.debug('Path de inicio : /')
-    return '<p>Hola Mundo<p>'
+    servidor_flask.logger.debug('Inicio : /')
+    return render_template("index.html", titulo = titulo_app)
 
 if __name__ == '__main__': # __name__ = nombre de este archivo
     servidor_flask.run(debug=True)

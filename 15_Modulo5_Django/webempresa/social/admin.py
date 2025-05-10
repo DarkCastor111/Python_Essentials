9ec0718301b36389394fcdbadba17ba3eb79234c
DarkCastor111 <tempo.cat13@gmail.com>
2025-05-10 22:43:40 +0100

    Version 2.5.12 Final

--- 15_Modulo5_Django/webempresa/social/admin.py
+++ 15_Modulo5_Django/webempresa/social/admin.py
@@ -4,6 +4,12 @@ from .models import Enlace
 # Register your models here.
 class EnlaceAdmin(admin.ModelAdmin):
     readonly_fields = ('fecha_creacion', 'fecha_actualizacion')
+
+    def get_readonly_fields(self, request, obj = None):
+        if request.user.groups.filter(name="Personal").exists():
+            return ('fecha_creacion', 'fecha_actualizacion', 'clave', 'nombre')
+        else:
+            return ('fecha_creacion', 'fecha_actualizacion')
     
 admin.site.register(Enlace, EnlaceAdmin)
 
